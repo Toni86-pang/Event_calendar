52524d07160fd5fc7d35b1702325cd53b01553ba
KariNurm <karinurm@gmail.com>
2023-07-07 11:47:44 +0300

    added  getevents, geteventbyid, geteventbyuserid

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ const server = express()
 server.use('/', express.static('./dist/client'))
 
 server.get('/version', (req: Request, res: Response) => {
-	res.send('Server version 0.2')
+	res.send('Server version 0.2.4')
 })
 
 server.use('/events', eventsRouter)
