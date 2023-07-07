4aa0ddfe8bd5d083624932a2dffe92b3b37fed74
“ni5u” <snissine@gmail.com>
2023-07-07 18:17:39 +0300

    moved version to server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
 import { authenticate, unknownEndpoint } from './middleware'
 import usersRouter from './routers/usersRouter'
 import eventsRouter from './routers/eventsRouter'
@@ -9,6 +9,9 @@ const server = express()
 server.use('/', express.static('./dist/client'))
 
 server.use(express.json())
+server.get('/version', (req: Request, res: Response) => {
+	res.send('Server version 0.2.7')
+})
 server.use('/users',  usersRouter)
 server.use('/events', authenticate, eventsRouter)
 server.use('/comments', authenticate, commentsRouter)
