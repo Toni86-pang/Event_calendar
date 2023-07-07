6f0a4181b65331f8741ce7c653b439ad521c0710
“ni5u” <snissine@gmail.com>
2023-07-07 18:25:43 +0300

    dockerfile updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,8 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import server from './server'
 
 
 server.use('/', express.static('./dist/client'))
-
-server.get('/version', (req: Request, res: Response) => {
-	res.send('Server version 0.2.6')
-})
 
 const PORT = process.env.PORT
 server.listen(PORT, () => console.log('Server pumping in port:', PORT))
