ecc2592c88b64f69605fc8c015ddc6191c602fc4
laurikummala <lauri.kummala@gmail.com>
2023-07-11 09:30:03 +0300

    Edited Event

--- frontend/src/components/event/Event.tsx
+++ frontend/src/components/event/Event.tsx
@@ -2,54 +2,54 @@ import { useLoaderData } from 'react-router-dom'
 import React, { useEffect, useState } from 'react'
 
 interface Event {
-    event_id?: string
-    title: string
-    content: string
-    isPrivate: boolean
-    date_time:string
+  event_id?: string
+  title: string
+  content: string
+  isPrivate: boolean
+  date_time: string
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function loader({ params }: any) {
 
-	return params.id
+  return params.id
 }
 
 export default function Event() {
 
-	const id = useLoaderData() as string
-	const [currentEvent, setCurrentEvent] = useState<Event | null>(null)
+  const id = useLoaderData() as string
+  const [currentEvent, setCurrentEvent] = useState<Event | null>(null)
 
-	useEffect(() => {
-		const getEventInfo = async () => {
-			try {
+  useEffect(() => {
+    const getEventInfo = async () => {
+      try {
 
-				const response = await fetch('api/events/event/' + id)
-				const event = await response.json() as Event[]
-				if (event.length > 0) {
-					setCurrentEvent(event[0])
-					console.log('All works')
-				} else {
-					setCurrentEvent(null)
-				}
-			} catch (error) {
-				console.log('Error fetching event data:', error)
-			}
-		}
+        const response = await fetch('api/events/event/' + id)
+        const event = await response.json() as Event[]
+        if (event.length > 0) {
+          setCurrentEvent(event[0])
+          console.log('All works')
+        } else {
+          setCurrentEvent(null)
+        }
+      } catch (error) {
+        console.log('Error fetching event data:', error)
+      }
+    }
 
-		getEventInfo()
+    getEventInfo()
 
-	}, [id])
+  }, [id])
 
-	return (
+  return (
 
-		<div className='events'>
-			<h3>
-                Title: {currentEvent && currentEvent.title}
-			</h3>
-			<p>Content: {currentEvent && currentEvent.content}</p>
-			<p>Private: {currentEvent && currentEvent.isPrivate}</p>
-			<p>Date and time: {currentEvent && currentEvent.date_time}</p>
-		</div>
-	)
+    <div className='events'>
+      <h3>
+        Title: {currentEvent && currentEvent.title}
+      </h3>
+      <p>Content: {currentEvent && currentEvent.content}</p>
+      <p>Private: {currentEvent && currentEvent.isPrivate}</p>
+      <p>Date and time: {currentEvent && currentEvent.date_time}</p>
+    </div>
+  )
 }
