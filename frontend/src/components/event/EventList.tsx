121e358aa5a83e88e01202f4de814a91799e2f5d
“ni5u” <snissine@gmail.com>
2023-07-11 16:51:45 +0300

    removed import react from eventlist

--- frontend/src/components/event/EventList.tsx
+++ frontend/src/components/event/EventList.tsx
@@ -1,5 +1,5 @@
 import { Link, Outlet } from 'react-router-dom'
-import React, { useState, ChangeEvent, useEffect } from 'react'
+import { useState, ChangeEvent, useEffect } from 'react'
 
 interface Event {
     event_id?: string
