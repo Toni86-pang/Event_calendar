c23ec543fc81c6d5e3d1af253658176f758851b6
KariNurm <karinurm@gmail.com>
2023-07-11 11:48:59 +0300

    login token to localstorage

--- frontend/src/components/nav/navbar.tsx
+++ frontend/src/components/nav/navbar.tsx
@@ -1,36 +1,36 @@
-import { Link, Outlet, NavLink } from 'react-router-dom';
-import { useState } from 'react';
+import { Link, Outlet, NavLink } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function Nav() {
-  const [loggedIn, setLoggedIn] = useState(false);
+	const [loggedIn, setLoggedIn] = useState(false)
 
-//   const handleLogin = () => {
-//     // Perform login logic
-//     setLoggedIn(true);
-//   };
+	//   const handleLogin = () => {
+	//     // Perform login logic
+	//     setLoggedIn(true);
+	//   };
 
-  const handleLogout = () => {
-    // Perform logout logic
-    setLoggedIn(false);
-  };
+	const handleLogout = () => {
+		// Perform logout logic
+		setLoggedIn(false)
+	}
 
-  return (
-    <div className='navbar'>
-      <nav>
-        <Link to={'/'}>home</Link>
-        <Link to='/events'>events</Link>
-        <NavLink to='/register' >
+	return (
+		<div className='navbar'>
+			<nav>
+				<Link to={'/'}>home</Link>
+				<Link to='/events'>events</Link>
+				<NavLink to='/register' >
           Register
-        </NavLink>
-        {loggedIn ? (
-          <button onClick={handleLogout}>Logout</button>
-        ) : (
-          <NavLink to='/login' >
+				</NavLink>
+				{loggedIn ? (
+					<button onClick={handleLogout}>Logout</button>
+				) : (
+					<NavLink to='/login' >
             Login
-          </NavLink>
-        )}
-      </nav>
-      <Outlet />
-    </div>
-  );
+					</NavLink>
+				)}
+			</nav>
+			<Outlet />
+		</div>
+	)
 }
