7e398558cf68cfe8686cab86c752798ade79a194
KariNurm <karinurm@gmail.com>
2023-07-11 16:45:56 +0300

    small changes and fun

--- frontend/src/components/nav/navbar.tsx
+++ frontend/src/components/nav/navbar.tsx
@@ -11,15 +11,16 @@ export default function Nav() {
 	}
 
 	return (
-		<div className='navbar'>
+		<div className='body'>
 			<nav>
-				<Link className='link' to={'/'}>Home</Link>
+				<Link className='link' to='/'>Home</Link>
 				<Link className='link' to='/events'>Events</Link>
 				<Link className='link' to='/register'>Register</Link>
-				{loggedIn ? <button onClick={handleLogout}>Logout</button>
+				{loggedIn ? <Link className='link' to='/' onClick={handleLogout}>Logout</Link>
 									: <Link className='link' to='/login'>Login</Link>
 				}
 			</nav>
+			<img className='construct' src='./src/img/pngwing.com.png' />
 			<Outlet context={setLoggedIn}/>
 		</div>
 	)
