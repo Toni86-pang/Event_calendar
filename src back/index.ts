3e98527bfa707bbf630402705d20c1a63065b69f
laurikummala <lauri.kummala@gmail.com>
2023-07-13 08:08:19 +0000

    Deleted src back and src front folders

--- src back/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import server from './server'
-
-
-server.use('/', express.static('./dist/client'))
-
-const PORT = process.env.PORT
-server.listen(PORT, () => console.log('Server pumping in port:', PORT))
